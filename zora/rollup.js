e3a8ef859784dcfb2b5d0121e3d6a6510123db38
Rasmus Schultz <rasmus@mindplay.dk>
2021-10-02 12:52:11 +0200

    avoid bundling `zora-reporters`: treat it as external (avoids duplication and chunks)

--- zora/rollup.js
+++ zora/rollup.js
@@ -4,20 +4,22 @@ import cjs from '@rollup/plugin-commonjs';
 export default {
   input: [
     './src/index.js',
+    './src/test.js',
     './src/harness.js',
+    './src/env.js',
   ],
   output: [
     {
       dir: './dist',
       format: 'cjs',
       entryFileNames: '[name].cjs',
-      chunkFileNames: '[name]-[hash].cjs'
     },
     {
       dir: './dist',
       format: 'es',
     },
   ],
+  external: ['zora-reporters'],
   treeshake: {
     moduleSideEffects: false, // otherwise package 'diff' from 'zora-reporters' gets included
   },
